211b9f47929ea229434f03f129a31077f1d8350f
kstarreveld <kstarreveld@deltion.nl>
2021-11-25 15:12:20 +0100

    Voorbeeld 1

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const MyContext = React.createContext("init");
+
+const  Child = () => {
+  const [own, setOwn] =useState("own");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{own}</Text>
+      <Text>in Child</Text>
+      <MyContext.Consumer>
+      { 
+        ({ data, counter})=>{setOwn(counter); return(
+         <View>
+           <Text>{data}</Text>
+           <Text>{counter}</Text>
+          </View>)}
+        
+      }
+      </MyContext.Consumer>
+
       <StatusBar style="auto" />
+    </View>);
+
+}
+
+export default function App() {
+  const [welcome, setWelcome] = useState("init");
+  const [counter, setCounter ] = useState(1);
+
+  return (
+    <View style={styles.container}>
+      <Text>{welcome}</Text>
+      <Button onPress={()=>{setWelcome("pressed"); setCounter(counter+1)}} title="test"/>
+      
+      <MyContext.Provider value={({ welcome,counter })}>
+          <Child/>
+      </MyContext.Provider>
+     
+     
+      <StatusBar style="auto" />
+     
     </View>
   );
 }
